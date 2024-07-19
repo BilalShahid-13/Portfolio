e2a30aa02a7cca9cd4b2d4bd2fe8a9819dcbbe2c
BilalShahid-13 <bcsm-f21-249@superior.edu.pk>
2024-07-19 15:04:43 +0500

    change Tmp file of constants

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -12,72 +12,67 @@ const Grid = ({ sendDataToParent }) => {
     setDataFromChild(data);
   }
 
+  console.log(Tmp)
+
   return (
     <>
       <div>
         {Tmp.map((itemss, index_) => (
           <div
             key={index_}
-            className={`flex flex-col
-           ${
-             index_ % 3 === 1
-               ? "justify-start items-start"
-               : "justify-end items-end"
-           }`}
+            className={`flex flex-col gap-9 mx-2
+           ${index_ % 2 === 0
+                ? "justify-start items-start"
+                : "justify-end items-end"
+              }`}
             id="card"
           >
-            {itemss.map((items, index) => (
-              <div key={index}>
-                <div
-                  className="bg-blue-50 rounded-md mx-2 
-                  hover:bg-blue-100 ease-linear duration-500 
+            <div
+              className="bg-blue-50 rounded-md mx-2
+                  hover:bg-blue-100 ease-linear duration-500
               transition-all cursor-pointer hover:shadow-md
                hover:shadow-blue-300 w-1/2 flex flex-col"
-                  id="grid"
-                  onClick={() => {
-                    sendDataToParent(index_);
-                    setclick_index(index_);
-                    settogglePopup(true);
-                    setDataFromChild(true);
-                  }}
-                >
-                  <img
-                    src={items.sampleimg}
-                    alt={items.sampleimg}
-                    className="px-8 h-full rounded-md drop-shadow-md"
-                  />
-                </div>
-                <div
-                  className={`flex flex-col ${
-                    index % 2 === 1
-                      ? "justify-start items-center"
-                      : "justify-end items-center"
-                  } w-full `}
-                >
-                  <h2
-                    className="dm-sans-font text-2xl font-bold
+              id="grid"
+              onClick={() => {
+                sendDataToParent(index_);
+                setclick_index(index_);
+                settogglePopup(true);
+                setDataFromChild(true);
+              }}
+            >
+              <img
+                src={itemss.sampleimg}
+                alt={itemss.sampleimg}
+                className="px-8 h-full rounded-md drop-shadow-md"
+              />
+            </div>
+            {/* title & des */}
+            <div
+              className={`flex flex-col ${index_ % 2 === 1
+                ? "justify-start items-center"
+                : "justify-end items-center"
+                } w-full `}
+            >
+              <h2
+                className="dm-sans-font text-2xl font-bold
                    text-zinc-600 mx-5"
-                  >
-                    {items.title}
-                  </h2>
-                  <p
-                    className="dm-sans-font text-sm font-light tracking-wide 
+              >
+                {itemss.title}
+              </h2>
+              <p
+                className="dm-sans-font text-sm font-light tracking-wide
                leading-snug px-4 text-justify text-zinc-500"
-                  >
-                    {items.des}
-                  </p>
-                </div>
-              </div>
-            ))}
+              >
+                {itemss.des}
+              </p>
+            </div>
           </div>
         ))}
         {dataFromChild === true ? (
           <main
-            // id="popup-"
             className={`ease-linear duration-700
-           transition-all ${
-             dataFromChild ? "h-screen w-full opacity-100" : "h-0 w-0 opacity-0"
-           } overflow-hidden`}
+           transition-all ${dataFromChild ? "h-screen w-full opacity-100" : "h-0 w-0 opacity-0"
+              } overflow-hidden`}
           >
             <ProjectjsShow
               sendDataToParent={handleDataFromChild}
--- src/Components/ProjectjsShow.jsx
+++ src/Components/ProjectjsShow.jsx
@@ -5,17 +5,24 @@ import Data from "../config/Data";
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 const ProjectjsShow = ({ sendDataToParent, index }) => {
-  const { Tmp, ProjectImages } = Data;
+  const { Tmp } = Data;
   const [arrofimg, setarrofimg] = useState([]);
   const [ImageTitle, setImageTitle] = useState([]);
 
   useEffect(() => {
     if (index >= 0) {
-      setarrofimg(ProjectImages[index].imgs);
-      setImageTitle(Tmp[index]);
+      console.log(Tmp[index])
+      setarrofimg(Tmp[index].imgs);
+      const tmpArray = Array.isArray(Tmp[index]) ? Tmp[index] : [Tmp[index]];
+      setImageTitle(tmpArray);
     }
-   
+
   }, [index]);
+
+  useEffect(() => {
+    console.log(ImageTitle)
+  }, [ImageTitle])
+
 
   return (
     <>
@@ -26,11 +33,11 @@ const ProjectjsShow = ({ sendDataToParent, index }) => {
         id="root-popup"
       >
         <div
-          className={`fixed bg-neutral-100 flex flex-col 
-        justify-start items-center z-[999] max-md:left-[5%] 
+          className={`fixed bg-neutral-100 flex flex-col
+        justify-start items-center z-[999] max-md:left-[5%]
         max-md:top-[5%] rounded-md shadow-md
         shadow-zinc-500  left-[5%] top-[5%]
-         overflow-auto ${"max-md:w-[90%] max-md:h-[90%"} 
+         overflow-auto ${"max-md:w-[90%] max-md:h-[90%"}
          w-[90%] h-[90%] ease-linear duration-300
           transition-all hide-scrollbar-webkit hide-scrollbar-moz`}
           id="popup"
@@ -38,7 +45,7 @@ const ProjectjsShow = ({ sendDataToParent, index }) => {
           <IoCloseCircleOutline
             size={35}
             className="fixed
-          max-md:right-12 max-md:top-12 z-50 cursor-pointer 
+          max-md:right-12 max-md:top-12 z-50 cursor-pointer
           hover:text-red-600 right-24 top-16"
             onClick={() => {
               sendDataToParent(false);
@@ -63,7 +70,7 @@ const ProjectjsShow = ({ sendDataToParent, index }) => {
             justify-center items-center text-justify px-4"
               >
                 <h6
-                  className="dm-sans-font 
+                  className="dm-sans-font
                 font-bold text-2xl text-zinc-600"
                 >
                   {items.title}
